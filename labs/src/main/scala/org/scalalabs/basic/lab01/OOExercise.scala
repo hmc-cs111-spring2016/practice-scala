5bd90a76306220329899ea91c918d72b97996e8d
Milo <mh042014@mymail.pomona.edu>
2016-02-09 18:08:35 -0800

    worked on first 2 exercises

--- labs/src/main/scala/org/scalalabs/basic/lab01/OOExercise.scala
+++ labs/src/main/scala/org/scalalabs/basic/lab01/OOExercise.scala
@@ -6,7 +6,7 @@ import scala.language.implicitConversions
  * Fix the code so that the unit test 'CurrencyExerciseTest' passes.
  *
  * In order for the tests to pass you need to do the following:
- * 
+ *
  * Exercise 1:
  * - Create a class Euro
  * - Provide it with two constructor parameters: euro:Int, cents:Int
@@ -15,7 +15,7 @@ import scala.language.implicitConversions
  * - Create an object Euro with a factory method named: fromCents that creates an Euro based on cents.
  * - Create a method named: + to the Euro class that adds another Euro
  * - Create a method named: * to the Euro class that multiplies an Euro by an integer
- * 
+ *
  * Exercise 2:
  * - Create an abstract class Currency
  * - Provide it with one constructor parameter: symbol:String
@@ -24,22 +24,57 @@ import scala.language.implicitConversions
  *   -> symbol + ': ' + euro + ',' + cents.  E.g: EUR 200,05
  * - In case the cents are 0 use this representation:
  *   -> symbol + ': ' + euro + ',--. E.g.: EUR 200,--
- *   
+ *
  * OPTIONAL: Exercise 3:
  * - Mix the Ordered trait in Euro
- * - Implement the compare method  
- * 
+ * - Implement the compare method
+ *
  * OPTIONAL: Exercise 4:
  * - Provide an implicit class that adds a *(euro:Euro) method to Int
  * - Create a new currency Dollar
- * - Provide a implicit conversion method that converts from Euro to Dollar using the 
+ * - Provide a implicit conversion method that converts from Euro to Dollar using the
  *   [[org.scalalabs.basic.lab01.DefaultCurrencyConverter]]
- * 
+ *
  * OPTIONAL: Exercise 5:
- * - Extend the conversion method from Euro to Dollar with an implicit parameter 
+ * - Extend the conversion method from Euro to Dollar with an implicit parameter
  *   of type [[org.scalalabs.basic.lab01.CurrencyConverter]]
- * - Use the implicit CurrencyConverter to do the conversion. 
+ * - Use the implicit CurrencyConverter to do the conversion.
  */
-class Euro {
+class Euro(val euro: Int, val cents: Int = 0) extends Currency("EUR"){
+  
+  lazy val inCents: Int = euro *100 + cents
+  
+  def +(otherEuro: Euro): Euro = {
+    
+    // This is the code I initially wrote :P  Goes to show how foreign Scala objects feel at the moment.
+    /* val totalCents = this.inCents + otherEuro.inCents
+     * new Euro(totalCents/100, totalCents%100)
+  	 */
+    
+    Euro.fromCents(inCents + otherEuro.inCents)
+  }
+  
+  def *(factor: Int): Euro = {
+    Euro.fromCents(inCents * factor)
+  }
+  
+  override lazy val toString = 
+    s"$symbol: $euro,${
+    if (cents > 0){
+      f"$cents%02d"
+    }
+    else {
+      "--"
+    }
+  }"
+}
 
+object Euro {
+  def fromCents(cents: Int): Euro = {
+    new Euro(cents/100, cents%100)
+  }
+}
+
+abstract class Currency(val symbol: String){
+  
 }
