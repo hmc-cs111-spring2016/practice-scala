f75bfb055234fc3ce2628ea25546a31cf9e01458
Joshua Landgraf <joshua@Joshs-MacBook-Pro-2.local>
2016-02-10 20:25:35 -0800

    Initial submission. Code should pass all unit tests

--- labs/src/main/scala/org/scalalabs/basic/lab01/HelloWorldExercise.scala
+++ labs/src/main/scala/org/scalalabs/basic/lab01/HelloWorldExercise.scala
@@ -25,7 +25,7 @@ object HelloWorld {
    * More on variable declarations can be found here:
    * http://programming-scala.labs.oreilly.com/ch02.html#VariableDeclarationsAndDefinitions
    */
-	val sayHello: String = "FixMe"
+	val sayHello: String = "Hello from Scala"
 
 
   /**
@@ -45,7 +45,7 @@ object HelloWorld {
    * More on method declarations can be found here:
    * http://programming-scala.labs.oreilly.com/ch02.html#MethodDeclarationsAndDefinitions
    */
-	def echo(text: String): String = "FixMe"
+	def echo(text: String): String = text
 }
 
 
@@ -79,7 +79,7 @@ object HelloWorld {
 object HelloWorldClassAndObject {
 	def apply(initialText:String):HelloWorldClassAndObject = {
 		new HelloWorldClassAndObject {
-			val text="FixMe"
+			val text = initialText
 		}
 	}
 }
@@ -99,11 +99,11 @@ object HelloWorldWithTraits extends HelloTrait with WorldTrait {
  * - combine the 'helloMethod' of HelloTrait and the 'worldMethod' of WorldTrait to create a new message
  * - just replacing the FixMe string would of course be cheating :)
  */
-	def hello:String = "FixMe"
+	def hello:String = helloMethod + " " + worldMethod
 }
 
 trait HelloTrait {
-	def helloMethod:String = "FixMe"
+	def helloMethod:String = "Hello"
 }
 
 trait WorldTrait {
